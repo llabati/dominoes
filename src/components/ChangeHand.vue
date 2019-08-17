6fbd46eeb23964f8f5da7fe716a9b202a0724f22
llabati <33926231+llabati@users.noreply.github.com>
2019-08-17 23:30:25 +0200

    Placement correct des dominos, tour après tour

--- src/components/ChangeHand.vue
+++ src/components/ChangeHand.vue
@@ -6,7 +6,9 @@
             </ul>
         </div>
         <div>
-            <button class="btn-play" @click="initGame">{{ name }}, launch the game</button>     
+
+            <button class="btn-play" @click="initGame">{{ name }}, launch the game</button>
+            
         </div>
     </div>
 </template>
@@ -54,24 +56,36 @@ export default {
         },
         chooseDomino(domino){
             domino.player = true
+            if(this.tour === 0) {
+                let head = domino.value[0]
+                let tail = domino.value[1]
+                domino.place = "left"
+                console.log('HEAD AND TAIL FIRST TOUR', head, tail)
+            }
             if (this.tour > 0) {
-                let head = this.$store.state.board[0].prev
-                let tail = this.$store.state.board[this.$store.state.board.length-1].next
+                let head = this.$store.state.board[0].value[0]
+                let tail = this.$store.state.board[this.$store.state.board.length-1].value[1]
                 console.log('HEAD AND TAIL PLAYER', head, tail)
-                domino.place = (domino.next === tail || domino.prev === tail) ? "right" : "left"
-                if (domino.place === "right") domino.tail = tail
-                else domino.head = head
+                domino.place = (domino.next === head || domino.prev === head) ? "left" : "right"
+                if (domino.place === "left") {
+                    domino.tail = undefined
+                    domino.head = head
+                } else {
+                    domino.head = undefined
+                    domino.tail = tail
+                }
             }
             console.log('MON DOMINO', domino)
             this.$store.commit('ADD_TO_BOARD', domino)
+            this.tour = ++this.tour
+            console.log('TOUR', this.tour)
             this.upto = !this.upto
-            this.tour = this.tour++
         },
         machinePlays(){
             if (this.$store.state.board.length > 0){
                 let choices = []
-                let head = this.$store.state.board[0].prev
-                let tail = this.$store.state.board[this.$store.state.board.length-1].next
+                let head = this.$store.state.board[0].value[0]
+                let tail = this.$store.state.board[this.$store.state.board.length-1].value[1]
                 console.log('HEAD AND TAIL', head, tail)
                 let one = this.$store.state.machineHand.filter(d => d.prev === head)
                 if (one) choices.push(one)
@@ -89,9 +103,17 @@ export default {
                 if (machineChoices.length === 0) console.log('JE PASSE !')
                 if (machineChoices.length === 1) {
                     machineChoices[0].player = false
+                    machineChoices[0].place = (machineChoices[0].next === head || machineChoices[0].prev === head) ? "left" : "right"
+            if (machineChoices[0].place === "left") {
+                machineChoices[0].tail = undefined
+                machineChoices[0].head = head
+            } else {
+                machineChoices[0].head = undefined
+                machineChoices[0].tail = tail
+            }
                     this.$store.commit('ADD_TO_BOARD', machineChoices[0])
                 }
-                if (machineChoices.length > 1) return this.makeChoice(machineChoices)
+                if (machineChoices.length > 1) return this.makeChoice(machineChoices, head, tail)
             }
         },
         makeChoice(machineChoices, head, tail){
@@ -110,11 +132,16 @@ export default {
             // la fonction chooseDomino doit être adaptée pour la machine (player = false doit être passé en argument, et pour le passage vers le joueur)
             
             let piece = this.$store.state.machineHand.find(p => p.value[0] === final[0] && p.value[1] === final[1])
-            console.log("LA PIECE A JOUER", piece)
+            console.log("LA PIECE A JOUER", piece, head, tail)
             piece.player = false
-            piece.place = (piece.next === tail || piece.prev === tail) ? "right" : "left"
-            if (piece.place === "right") piece.tail = tail
-            else piece.head = head
+            piece.place = (piece.next === head || piece.prev === head) ? "left" : "right"
+            if (piece.place === "left") {
+                piece.tail = undefined
+                piece.head = head
+            } else {
+                piece.head = undefined
+                piece.tail = tail
+            }
             this.$store.commit('ADD_TO_BOARD', piece)
         }
     }
