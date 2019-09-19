ed76b1148460973b968f2b01fcfde1f0bd53ba72
llabati <33926231+llabati@users.noreply.github.com>
2019-09-19 13:01:39 +0200

    Installation du drag and drop

--- src/services/utils.js
+++ src/services/utils.js
@@ -3,11 +3,18 @@ export default {
         let randomDraw = Math.floor(Math.random() * 28)
         return randomDraw
     },
-    
     // inverse la gauche et la droite du domino
     swap: function(doubleValue){
+        console.log('UTILS SWAP!!!', doubleValue)
         let t = doubleValue[0]
         doubleValue[0] = doubleValue[1]
         doubleValue[1] = t
+        console.log('UTILS SWAP', doubleValue)
+    },
+    setScores(rest) {
+        return rest.reduce((sum, a) => sum + a.value[0] + a.value[1], 0)
+    },
+    incrementIndex(i){
+        return i++
     }
 }
