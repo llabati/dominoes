c5ad821e813f344fb1d06871c1fd2dca236eed25
llabati <33926231+llabati@users.noreply.github.com>
2019-10-28 22:32:17 +0100

    Adoption du click haut / bas sur le domino

--- src/components/HalfDomino.vue
+++ src/components/HalfDomino.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class="half-domino">
-        <div class="row-in-domino">
+    <div class="half-domino" @click="choose">
+        <div class="img-container">
+            <img class="domino-value" v-if="value === 0" src="../assets/0.png">
+            <img class="domino-value" v-if="value === 1" src="../assets/1.png">
+            <img class="domino-value" v-if="value === 2" src="../assets/2.png">
+            <img class="domino-value" v-if="value === 3" src="../assets/3.png">
+            <img class="domino-value" v-if="value === 4" src="../assets/4.png">
+            <img class="domino-value" v-if="value === 5" src="../assets/5.png">
+            <img class="domino-value" v-if="value === 6" src="../assets/6.png">
+        </div>
+        <!--<div class="row-in-domino">
             <div class="surdot">
             <div class="dot" v-if="value === 4 || value === 5 || value === 6"></div></div>
             <div class="surdot"></div>
@@ -15,7 +24,7 @@
             <div class="surdot"><div class="dot" v-if="value === 2 || value === 3 || value === 4 || value === 5 || value === 6"></div></div>
             <div class="surdot"></div>
             <div class="surdot"><div class="dot" v-if="value === 4 || value === 5 || value === 6"></div></div>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -23,6 +32,11 @@
 export default {
     props: {
         value: Number
+    },
+    methods: {
+        choose(){
+            this.$emit('chosen')
+        }
     }
 }
 </script>
@@ -40,7 +54,10 @@ export default {
 }
 .row-in-domino {
     height: 24px;
+    /*display: flex;
+    flex-direction:row;*/
     padding: 2px;
+    /*justify-content: space-around;*/
 }
 .surdot {
     display: inline-block;
@@ -54,4 +71,10 @@ export default {
     margin: 0 auto;
 }
 
+.img-container {
+    padding: 0;
+}
+.domino-value {
+    width: 100%;
+}
 </style>
